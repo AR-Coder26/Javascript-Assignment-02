2ea773831ca4d63510e16a24306ed65cf4596bff
Harry-Developer156 <ammadcarexport@gmail.com>
2026-02-14 13:57:45 +0500

    Task 1.1 – map vs forEach vs reduce (Behavior Study) completed

--- src/app.js
+++ src/app.js
@@ -50,10 +50,44 @@ buttons.forEach((btn) => {
     }
   });
 });
-                // Section 1: Array Advanced Methods (Deep Practice)
+// Section 1: Array Advanced Methods (Deep Practice)
 /////////////////////////////////////////////////////////////////////////////////////////////////
-                // Task 1.1 – map vs forEach vs reduce (Behavior Study)
+// f
 /////////////////////////////////////////////////////////////////////////////////////////////////
-function task1_1(){
-
-};
+function task1_1() {
+  // Map, reduce, Higher Order Array Methods
+  console.clear();
+  console.log("==================== Task 1.1 ====================");
+  console.log("========== Using Map ==========");
+  // Map hr element par function () => {} apply karta hai aur nayi list return karta hai. map javascript ka builtin function hai array object k liye
+  let arr = [25, 45, 65, 85];
+  let newMap = arr.map((value, index, array) => {
+    // value, index, array is parameter of map() function
+    console.log(`Item = ${value}, Index = ${index}, Original Array = ${array}`);
+    return value + 10; // return map k function sy bhr value value bhjta hai current value mei plus 10 kr k
+  });
+  console.log(`New Array = ${newMap}`);
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Reduce array ke har element par function chalata hai aur ek single final value return karta hai. yeh sirf array par chalta hai. Iska kaam ye hai k ye bohat saari cheezon ko mila kar ek final result bnata hai. Reduce boly tw MCU ka ant-man pym pertical sy chotta hojata hai
+  console.log("========== Using Reduce ==========");
+  // accumulator boly tw Wo variable jo her iteration ke baad updated result store karta rehta hai.
+  // current boly tw Wo element jo abhi process ho raha hota hai.
+  let newReduce = arr.reduce((accumulator, current) => {
+    console.log(
+      `Previous storage value = ${accumulator}, current value = ${current}`,
+    );
+    // previous stored value + current value = reduce result
+    return accumulator + current;
+  }, 0);
+  console.log(`Reduce result = ${newReduce}`);
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  console.log("========== Using ForEach ==========");
+  // ForEach array ke hr element par function chala k sirf side-effect (console.log) karta hai, koi nayi array return nahi karta
+  arr.forEach((value, index, array) => {
+    // simple print hr element ka
+    console.log(`Item = ${value}, Index = ${index}, Original Array = ${array}`);
+  });
+  console.log(
+    "ForEach me koi nayi array return nahi hoti, sirf hr km k liye loop chalta hai",
+  );
+}
